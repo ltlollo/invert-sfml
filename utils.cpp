4ab958fd12f0721c17b788a40b873e4b7a7dd897
ltlollo <ltlollo.it@gmail.com>
2014-08-13 15:10:31 +0200

    readd utils.cpp

---
+++ utils.cpp
@@ -0,0 +1,15 @@
+#include "utils.h"
+
+using namespace std;
+
+template<> void getnum<int>(const char* str, int& num) {
+    num = atoi(str);
+}
+
+template<> void getnum<float>(const char* str, float& num) {
+    num = atof(str);
+}
+
+template<>  void getnum<long>(const char* str, long& num) {
+    num = atol(str);
+}
